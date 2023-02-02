769413d68457450bbbbe15ef82b8279cf18c9f77
Nicolas <nicobouvet7@gmail.com>
2023-02-02 19:52:45 -0300

    .

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {getRecipes } from "../../redux/actions";
-import CardsContainer from "../../components/CardContainer/CardsContainer.jsx";
+import CardsContainer from "../../components/CardsContainer/CardsContainer.jsx";
 
 
 
