e37f27b8fbb53364f094e8e47d616f280f31a5cd
Nicolas <nicobouvet7@gmail.com>
2023-02-06 17:17:10 -0300

    .

--- client/src/components/SearchBar.jsx/SearchBar.jsx
+++ client/src/components/SearchBar.jsx/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
 import { getRecipeByName } from "../../redux/actions";
 import LoadingSearch from "./loadingSearch";
 import style from "./SearchBar.module.css";
@@ -9,7 +8,7 @@ import style from "./SearchBar.module.css";
 
 
 export const SearchBar=({currentRoute})=>{
-    location=useLocation()
+
 
     const dispatch=useDispatch();
     
